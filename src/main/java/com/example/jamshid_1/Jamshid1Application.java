2e00e95dcdac5c3c5e8a11f9ff536795ed02e675
jamshid <jmadaminov.unlimited@gmail.com>
2024-10-21 23:27:15 +0500

    feat: jammshid added

--- src/main/java/com/example/jamshid_1/Jamshid1Application.java
+++ src/main/java/com/example/jamshid_1/Jamshid1Application.java
@@ -9,6 +9,7 @@ public class Jamshid1Application {
     public static void main(String[] args) {
         SpringApplication.run(Jamshid1Application.class, args);
         System.out.println("Hello");
+        System.out.println("jamshid");
     }
 
 
