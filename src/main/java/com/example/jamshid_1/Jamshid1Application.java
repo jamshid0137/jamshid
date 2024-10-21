ca5b2bea01aa29cdcc57385f2a92949cea9845ca
jamshid <jmadaminov.unlimited@gmail.com>
2024-10-21 23:19:11 +0500

    inital commit

---
+++ src/main/java/com/example/jamshid_1/Jamshid1Application.java
@@ -0,0 +1,13 @@
+package com.example.jamshid_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jamshid1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jamshid1Application.class, args);
+    }
+
+}
