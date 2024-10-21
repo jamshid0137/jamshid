4fce5ad06035bd376d49f352a4a61b676a9cb782
jamshid <jmadaminov.unlimited@gmail.com>
2024-10-21 23:24:24 +0500

    feat: Hello word added

--- src/main/java/com/example/jamshid_1/Jamshid1Application.java
+++ src/main/java/com/example/jamshid_1/Jamshid1Application.java
@@ -8,6 +8,8 @@ public class Jamshid1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Jamshid1Application.class, args);
+        System.out.println("Hello");
     }
+
 
 }
